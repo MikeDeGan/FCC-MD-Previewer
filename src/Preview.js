ffa08067648691b72cf089bcae94d4b3e9f900e6
Mike DeGan <mikedegan@gmail.com>
2019-01-02 21:16:23 -0500

    added flexbox, moved startup text, styled tables, added code colorizer.

--- src/Preview.js
+++ src/Preview.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import marked from 'marked';
+import highlightjs from 'highlight.js';
 
 const Preview = props => {
+  // Custom renderer to handle a tags and code colorizing
   const renderer = new marked.Renderer();
   renderer.link = function(href, title, text) {
     return `<a target="_blank" href="${href}">${text}</a>`;
+  };
+  renderer.code = (code, language) => {
+    // Check whether the given language is valid for highlight.js.
+    const validLang = !!(language && highlightjs.getLanguage(language));
+    // Highlight only if the language is valid.
+    const highlighted = validLang
+      ? highlightjs.highlight(language, code).value
+      : code;
+    // Render the highlighted code with `hljs` class.
+    return `<pre><code class="hljs ${language}">${highlighted}</code></pre>`;
   };
 
   function rawMarkup() {
